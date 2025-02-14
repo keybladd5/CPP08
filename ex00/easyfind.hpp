a2291ddd4c0543fb0c98469ce6a66cce227cbf11
Pol Martínez Badenes <polmartinex@gmail.com>
2025-02-14 17:32:43 +0100

    rm: eval subject leaked as comment deleted

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -29,25 +29,3 @@ typename T::iterator easyfind(T &cont, int &to_find)
 }
 
 #endif
-/*There is a class that complies with the requirements of the subject.
-Its member functions use STL algorithms to find their result as much as
-possible. Finding the shortest span can't be done only by subtracting
-the two lowest numbers (take a closer look at the subject example).
-
-Improved addNumber function
-There's a way to add numbers that's more practical than calling the
-addNumber() function repeatedly.
-
-Exercise 02: Mutated abomination
-As usual, there has to be enough tests to prove the program works as expected. If there isn't, 
-do not grade this exercise. If any non-interface class is not in orthodox canonical class form, do not grade this exercise.
-
-MutantStack class
-There is a MutantStack class that offers all of std::stack methods.
-It must contain an iterator.
-Also, it is possible to do at least the operations shown in the subject's examples using iterators.
-
-Better tests
-There is a at least a main() function that has more tests than the ones
-from the subject.*/
-
